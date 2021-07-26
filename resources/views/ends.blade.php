94e657b87027570a0d13995efc454ddb6d52f3ec
Akbar_Ibrahim <akbardevutil@gmail.com>
2021-07-26 16:07:21 +0100

    changed background color to black

--- resources/views/ends.blade.php
+++ resources/views/ends.blade.php
@@ -5,7 +5,7 @@
 <div class="w3-container" style="width: 90%; margin: auto;">
 
 <div class="w3-container w3-margin w3-padding">
-        <h1 style="font-size: 50px;"> <b> Ends With </b> </h1>
+        <h1 style="font-size: 30px;"> <b> Ends With </b> </h1>
     </div>
 
     <div class="w3-container w3-margin w3-padding">
@@ -14,14 +14,14 @@
 
     <div class="w3-container w3-center w3-hide-small">
         @foreach($letters as $letter)
-        <div letter="{{ strtoupper($letter) }}" class="letter w3-padding w3-black w3-hover-amber w3-text-white w3-circle w3-left"
+        <div letter="{{ strtoupper($letter) }}" class="letter w3-padding w3-black w3-hover-amber w3-text-white w3-left"
             style="font-size: 60px; width: 120px; color: white; cursor: pointer;"> <b>{{ strtoupper($letter) }}</b> </div>
         @endforeach
     </div>
 
     <div class="w3-container w3-center w3-hide-large">
         @foreach($letters as $letter)
-        <div letter="{{ strtoupper($letter) }}" class="letter w3-padding w3-black w3-hover-amber w3-text-white w3-circle w3-left"
+        <div letter="{{ strtoupper($letter) }}" class="letter w3-padding w3-black w3-hover-amber w3-text-white w3-left"
             style="font-size: 30px; width: 60px; color: white;"> <b>{{ strtoupper($letter) }}</b> </div>
         @endforeach
     </div>
